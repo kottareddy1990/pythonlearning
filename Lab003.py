168160fdb52194392f5b39408d5f23358cd40e6d
kottareddy1990 <kotta.reddy1990@gmail.com>
2024-08-02 16:50:03 +0530

    Add files via upload

---
+++ Lab003.py
@@ -0,0 +1,7 @@
+name = input("Enter yout First name ")
+name1= input("Enter your second name")
+#name2 = input("Enter your Second name ")
+#name = ("name1", "name2",)
+print(name,name1, sep="-",end="\t")
+
+
